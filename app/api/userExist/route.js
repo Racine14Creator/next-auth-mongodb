efec821c80e4b6106adbc930e4feccfae711792e
Webmaster019 <kingbestd030@gmail.com>
2024-06-11 11:42:32 +0300

    Fix login error

--- app/api/userExist/route.js
+++ app/api/userExist/route.js
@@ -7,7 +7,7 @@ export async function POST(req) {
     await connectMongoDB();
     const { email } = await req.json();
     const user = await User.findOne({ email }).select("_id");
-    console.log("user 11: ", user);
+    console.log("user: ", user);
     return NextResponse.json({ user });
   } catch (error) {}
 }
